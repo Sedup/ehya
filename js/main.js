ccb2d4c53bc65cd31fee9c7bf41a80841d41b498
Sedup <69250193+Sedup@users.noreply.github.com>
2020-10-17 16:31:32 +0300

    add: tabs in trends section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-var menuButton = $(".navbar-top__menu-button");
-menuButton.on("click", function () {
-  $(".navbar-bottom").toggleClass("navbar-bottom__mobile--visible");
-});
+$(document).ready(function () {
+  var menuButton = $(".navbar-top__menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom__mobile--visible");
+  });
 
-var closeButton = $(".navbar-bottom__menu-close");
-closeButton.on("click", function () {
-  $(".navbar-bottom").toggleClass("navbar-bottom__mobile--visible");
+  var closeButton = $(".navbar-bottom__menu-close");
+  closeButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom__mobile--visible");
+  });
+
+  var tabsItem = $(".trands-menu__tabs-item");
+  var cardsItem = $(".trands-cards__item");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trands-menu__tabs-item-active");
+    $(this).addClass("trands-menu__tabs-item-active");
+
+
+    if (activeContent == "best") {
+      $(cardsItem).css('order', '');
+      for (i = 6, j = 1; i >= 0; i--) {
+        $(cardsItem[i]).css('order', j);
+        j++;
+      };
+    }
+    else if (activeContent == "soon") {
+      $(cardsItem).css('order', '');
+      for (i = 0; i < 7; i++) {
+        let j = Math.floor(Math.random() * (20 - 1)) + 1;
+        $(cardsItem[i]).css('order', j);
+      };
+    }
+    else $(cardsItem).css('order', '');
+  });
+
 });
