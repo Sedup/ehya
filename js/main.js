22d20862a7bc5342cb1246807896bdc01c84d148
Sedup <69250193+Sedup@users.noreply.github.com>
2020-10-17 19:40:27 +0300

    Create: reviews section

--- js/main.js
+++ js/main.js
@@ -35,4 +35,22 @@ $(document).ready(function () {
     else $(cardsItem).css('order', '');
   });
 
+  //Slider
+
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    autoplay: {
+      delay: 5000,
+    },
+  })
+
 });
